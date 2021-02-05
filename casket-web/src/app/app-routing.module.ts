60218003b1991b681b9f14d9308d32b0e8676b3c
jasoc <paridegiunta@gmail.com>
2021-02-05 19:20:47 +0100

    Rotte

--- casket-web/src/app/app-routing.module.ts
+++ casket-web/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { disconnect } from 'process';
-import { callbackify } from 'util';
 import { HomeComponent } from 'src/app/home/home.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
 ];
 
 @NgModule({
